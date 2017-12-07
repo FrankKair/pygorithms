21bf0fcfc483cc7b2ef6b4f8e9a02a212765deec
Frank Kair <frankkair@gmail.com>
2017-12-07 16:25:51 -0200

    Adds non in-place Selection Sort

--- sorting/selection_sort.py
+++ sorting/selection_sort.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 
-def selection_sort(list):
+def selection_sort_in_place(list):
     for i in range(0, len(list) - 1):
         minimum = i
         for j in range(i + 1, len(list)):
@@ -12,6 +12,23 @@ def selection_sort(list):
         list[minimum] = temp
 
 
-list = [2, 7, 4, 1, 5, 3]
-selection_sort(list)
-print list
+def selection_sort(list):
+    list = list
+    for i in range(0, len(list)):
+        min_index = i
+        for j in range(i + 1, len(list)):
+            if list[j] < list[min_index]:
+                min_index = j
+        temp = list[i]
+        list[i] = list[min_index]
+        list[min_index] = temp
+    return list
+
+
+# Examples
+list_to_sort = [2, 7, 4, 1, 5, 3]
+selection_sort_in_place(list_to_sort)
+print list_to_sort
+
+sorted_list = selection_sort([2, 7, 4, 1, 5, 3])
+print sorted_list
