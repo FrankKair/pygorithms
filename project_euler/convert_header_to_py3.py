1ea1a84ba95b5ad765815aaa4afad06de5bf90b7
Frank Kair <frankkair@gmail.com>
2017-12-28 13:34:30 -0200

    Adds convert_header_to_py3 script

---
+++ project_euler/convert_header_to_py3.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import os
+
+
+def convert_header_to_python3():
+    directory = os.path.dirname(__file__)
+
+    for filename in os.listdir(directory):
+        if filename.endswith(".py") and filename.startswith("0"): 
+            lines = []
+            with open(os.path.join(directory, filename), 'r+') as file:
+                lines = file.readlines()[1:]
+                file.truncate()
+
+            with open(os.path.join(directory, filename), 'r+') as file:
+                file.writelines(['#!/usr/bin/env python3', '\n'] + lines)
+
+        else:
+            continue
+
+
+convert_header_to_python3()
