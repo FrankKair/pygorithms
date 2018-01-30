f1b8976f8b4e7f7cb43538677ca1df130eb0e7ec
Frank Kair <frankkair@gmail.com>
2018-01-29 23:20:37 -0200

    Adds comments to print euler script

--- project_euler/print_all_euler.py
+++ project_euler/print_all_euler.py
@@ -8,19 +8,25 @@ def print_all_euler_results():
     os.chdir(dir_path)
 
     for filename in os.listdir(dir_path):
-        if filename.endswith(".py") and filename.startswith("0"):            
+        # len(filename) == 6 --> example: 001.py
+        if filename.endswith(".py") and len(filename) == 6:
+
             command = "python3 {file}".format(file=filename)
 
+            # Typecast to 'str' because check_output() returns 'bytes'
             output = str(check_output(command, shell=True))
-            l = [int(x) for x in output if x in ['0','1','2','3','4','5','6','7','8','9']]
+
+            # output example -> b'233168\n'
+            l = [
+                int(x) for x in output
+                if x in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+            ]
             result = ''.join(map(str, l))
 
             problem_number = filename.replace('.py', '')
 
             out = ("Problem {number}: {result}").format(
-                number=problem_number,
-                result=result
-            )
+                number=problem_number, result=result)
 
             print(out)
 
