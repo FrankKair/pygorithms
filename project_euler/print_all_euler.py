389252b441cda1044dceee0d68021c71d944dfd0
Frank Kair <frankkair@gmail.com>
2018-01-29 23:10:00 -0200

    Adds script to print out all euler results

---
+++ project_euler/print_all_euler.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import os
+from subprocess import check_output
+
+
+def print_all_euler_results():
+    current_working_directory = os.getcwd()
+
+    for filename in os.listdir(dir_path):
+        if filename.endswith(".py") and filename.startswith("0"):            
+            command = "python3 {file}".format(file=filename)
+
+            output = str(check_output(command, shell=True))
+            l = [int(x) for x in output if x in ['0','1','2','3','4','5','6','7','8','9']]
+            result = ''.join(map(str, l))
+
+            problem_number = filename.replace('.py', '')
+
+            out = ("Problem {number}: {result}").format(
+                number=problem_number,
+                result=result
+            )
+
+            print(out)
+
+
+print_all_euler_results()
