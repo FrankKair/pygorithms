c40ebf1d739260ed9468b210827c2eab0b1a3b1c
Frank Kair <frankkair@gmail.com>
2018-02-20 02:08:13 -0300

    Adds hacker rank directory

---
+++ hacker_rank/algorithms/CamelCase.py
@@ -0,0 +1,12 @@
+#!/bin/python3
+import sys
+
+
+def camelcase(s):
+    return len([char for char in s if char.isupper()]) + 1
+
+
+if __name__ == "__main__":
+    s = input().strip()
+    result = camelcase(s)
+    print(result)
