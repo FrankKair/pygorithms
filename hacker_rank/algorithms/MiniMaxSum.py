23eed2c279f2e102977354f414f051ad45673eca
Frank Kair <frankkair@gmail.com>
2018-02-22 00:11:10 -0300

    Adds MiniMaxSum problem

---
+++ hacker_rank/algorithms/MiniMaxSum.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import sys
+from itertools import permutations
+
+
+def miniMaxSum(arr):
+    max_product = sum(max(permutations(arr, 4)))
+    min_product = sum(min(permutations(arr, 4)))
+    print(min_product, max_product)
+    
+    
+if __name__ == "__main__":
+    arr = list(map(int, input().strip().split(' ')))
+    miniMaxSum(arr)
