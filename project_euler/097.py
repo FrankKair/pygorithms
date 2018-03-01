f555b80aac77530cc48aee146c27417bd9fd431e
Frank Kair <frankkair@gmail.com>
2018-02-28 21:48:12 -0300

    Refactors 097 using modular arithmetic

--- project_euler/097.py
+++ project_euler/097.py
@@ -1,18 +1,6 @@
-# !/usr/bin/env python3
+#!/usr/bin/env python3
 
 
-exp = 7830457
-big_n = 2 ** exp
-n = 28433 * big_n + 1
-
-
-def digits(n):
-    return [int(n) for n in str(n)]
-
-
-digits = digits(n)
-last_ten_slice = slice(-10, None)
-result = digits[last_ten_slice]
-
-
+# Modular arithmetic
+result = (28433 * pow(2, 7830457, 10**10) + 1) % 10**10 
 print(result)
