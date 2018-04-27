0b22401b7501b804b6c578718ac0883eec40c189
Frank Kair <frankkair@gmail.com>
2018-04-27 16:08:47 -0300

    Refactor: Simplifies 022
    File loading function already strips backslash.

--- project_euler/022.py
+++ project_euler/022.py
@@ -2,28 +2,22 @@
 import os
 
 
+def names():
+    file = open(os.path.join(os.path.dirname(__file__), 'p022_names.txt'))
+    names_list = file.read().replace('\"', '').split(',')
+    return sorted(names_list)
+
+
 def name_score(name):
     return sum(ord(char) - 64 for char in name)
 
 
-def remove_quotes(string):
-    return string.strip('\"')
-
-
-def names():
-    file = open(os.path.join(os.path.dirname(__file__), 'p022_names.txt'))
-    return sorted(file.read().split(','))
-
-
 def solve():
     names_list = names()
-
     total_score = 0
     for index, name in enumerate(names_list):
-        name = remove_quotes(name)
         score = name_score(name) * (index + 1)
         total_score += score
-
     return total_score
 
 
