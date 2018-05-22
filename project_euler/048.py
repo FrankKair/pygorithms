822f798184feaeaed156277e37e84892f3f6e708
Frank Kair <frankkair@gmail.com>
2018-05-22 17:59:22 -0300

    Feat: Adds Euler 048

---
+++ project_euler/048.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+def self_powers(limit):
+    return sum((x ** x for x in range(1, limit)))
+
+
+def solve():
+    mod = 10 ** 10
+    return self_powers(1001) % mod
+
+
+if __name__ == '__main__':
+    result = solve()
+    print(result)
