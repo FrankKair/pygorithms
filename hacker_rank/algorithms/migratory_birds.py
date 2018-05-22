9da3f765a4165e9ff988947698bd58fda6dcbe13
Frank Kair <frankkair@gmail.com>
2018-05-21 22:48:30 -0300

    Refactor: Uses Counter data structure to count

--- hacker_rank/algorithms/migratory_birds.py
+++ hacker_rank/algorithms/migratory_birds.py
@@ -1,16 +1,13 @@
 #!/bin/python3
 # https://www.hackerrank.com/challenges/migratory-birds/problem
+from collections import Counter
 
 
-def migratory_birds(n, ar):
-    frequency_list = [0] * (n + 1)
-    for index in ar:
-        frequency_list[index] += 1
-    return frequency_list.index(max(frequency_list))
+def migratory_birds(ar):
+    return Counter(ar).most_common(1)[0][0]
 
 
 if __name__ == '__main__':
-    n = 6
     ar = [1, 4, 4, 4, 5, 3]
-    result = migratory_birds(n, ar)
+    result = migratory_birds(ar)
     print(result)
