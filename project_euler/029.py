eefee10119cc1276de32af4d143745d072098969
Frank Kair <frankkair@gmail.com>
2018-05-23 00:38:50 -0300

    Feat: Adds Euler 029

---
+++ project_euler/029.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import itertools
+
+
+def solve():
+    # Generates set of a ^ b for 2 ≤ a ≤ 100 and 2 ≤ b ≤ 100
+    return len({a**b for a in range(2, 101) for b in range(2, 101)})
+
+
+if __name__ == '__main__':
+    result = solve()
+    print(result)
