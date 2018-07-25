6cdb8036b67e324b5a5b77f0261ac97b15b3c46e
Frank Kair <frankkair@gmail.com>
2018-07-25 18:36:56 -0300

    Fixes spacing (4)

--- project_euler/035.py
+++ project_euler/035.py
@@ -7,9 +7,9 @@ def list_primes_below(n):
 	result = [True] * (n + 1)
 	result[0] = result[1] = False
 	for i in range(int(sqrt(n) + 1)):
-		if result[i]:
-			for j in range(i * i, len(result), i):
-				result[j] = False
+            if result[i]:
+                for j in range(i * i, len(result), i):
+		    result[j] = False
 	return result
 
 
@@ -17,8 +17,8 @@ def solve():
 	primes = list_primes_below(999999)
 	
 	def is_circular_prime(n):
-		s = str(n)
-		return all(primes[int(s[i : ] + s[ : i])] for i in range(len(s)))
+	    s = str(n)
+    	    return all(primes[int(s[i : ] + s[ : i])] for i in range(len(s)))
 	
 	return sum(1 for i in range(len(primes)) if is_circular_prime(i))
 
